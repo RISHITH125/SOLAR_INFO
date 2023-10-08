38f05494e81cbf4ac43d4d04ff125f3798ff3583
rahul <rahul.baradol.14@gmail.com>
2023-10-08 14:17:26 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex items-center justify-center bg-blue-600 w-[400px] h-[100px]'>
+        Time to start the solar info project!!
+      </div>
+    </>
+  )
+}
+
+export default App
