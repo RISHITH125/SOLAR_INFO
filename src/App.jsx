dfec504ac80258ab8526b15b8752eb86c7d616ac
RISHITH125 <rishithp125@gmail.com>
2023-10-10 12:44:54 +0530

    Added the system!

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import './App.css'
+import System from './system/system'
 
 function App() {
 
   return (
     <>
-      <div className='flex items-center justify-center bg-blue-600 w-[400px] h-[100px]'>
-        Time to start the solar info project!!
-      </div>
+      <System />
     </>
   )
 }
---
+++ src/system/System.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './system.css'
+
+const System = () => {
+  return (
+    <>
+        <div>
+            <div className="sun"></div>
+            <div className="mercury"></div>
+            <div className="venus"></div>    
+            <div className="earth"></div> 
+            <div className="mars"></div> 
+        </div>
+    </>
+  )
+}
+
+export default System
