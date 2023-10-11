27a0d9af6a9b127fff1b4268cc57bcb1179c3e5f
RISHITH125 <rishithp125@gmail.com>
2023-10-11 17:54:12 +0530

    added homescree and scroll wheel interface

--- src/system/System.jsx
+++ src/system/System.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './system.css'
+import { useRef } from 'react'
 
 const System = () => {
-  return (
+  const main = useRef(null); // main is like a pointer | main.current = *main
+  const [scaleFactor, setScaleFactor] = useState(1100);
+  const [translateFactor, setTranslateFactor] = useState(-3);
+  const [opacity, setOpacity] = useState(1);
+
+  let mainStyle = {
+    transform: `scale(${scaleFactor}%) translateX(${translateFactor}%)`
+  }
+
+  let opacityClass = {
+    opacity: `${opacity}`
+  }
+
+  useEffect(()=>{
+    main.current.addEventListener('wheel', (e)=>{
+        if (e.deltaY > 0) {
+          setScaleFactor((current) => {
+            if (current < 100) return current;
+            setTranslateFactor((c) => c + 0.009);
+            setOpacity(o => o - 0.01)
+            return current - 5;
+          })
+        } else {
+          setScaleFactor((current) => {
+            if (current < 100 || current > 1100) return current;
+            setTranslateFactor((c) => c - 0.009);
+            setOpacity(o => o + 0.01)
+            return current + 5;
+          })
+        }
+    })
+  }, [main])
+
+  return ( 
     <>
-        <div className="main">
-            <div className="sun"></div>
+        <div ref={main} className={`main`} style={mainStyle}>
+            <div className="sun flex items-center">
+              <h1 className={`relative font-thin text-orange-400 left-[3vw]`} style={opacityClass}>SOLAR</h1>
+              <h1 className={`relative text-white left-[4vw] info`} style={opacityClass}>INFO</h1>
+            </div>
             <div className="mercury"></div>
             <div className="venus"></div>    
             <div className="earth"></div> 
