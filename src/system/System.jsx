c831e2449e17f081a22abfa131dc1b7a527a21fc
RISHITH125 <rishithp125@gmail.com>
2023-10-23 00:14:37 +0530

    made the planetary system responsive, added separation variables and lotmore

--- src/system/System.jsx
+++ src/system/System.jsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 const System = () => {
   const main = useRef(null); // main is like a pointer | main.current = *main
   const [scaleFactor, setScaleFactor] = useState(1100);
-  const [translateFactor, setTranslateFactor] = useState(-3);
+  const [translateFactor, setTranslateFactor] = useState(-2);
   const [opacity, setOpacity] = useState(1);
 
   let mainStyle = {
@@ -41,56 +41,55 @@ const System = () => {
   }, [main])
 
   return ( 
-    <div className='overflow-hidden'>
-        <div ref={main} className={`main`} style={mainStyle}>
-            <div className="sun flex items-center">
+    <div className=''>
+        <div ref={main} className={`main flex justify-center items-center`} style={mainStyle}>
+            <div className="sun flex absolute items-center">
               <h1 className={`relative font-thin text-white left-[3vw] info`} style={opacityClass}>SOLAR</h1>
               <h1 className={`relative text-white left-[4vw] info`} style={opacityClass}>INFO</h1>
-            
-            <Link to="/sun">
-            <div className="sun-shadow"></div>
-            </Link>
+              <Link to="/sun">
+              <div className="sun-shadow"></div>
+              </Link>
             </div>
+            <div className='absolute'>
+                <Link to="/mercury">
+                  <div className="mercury"></div>	
+                </Link>
 
+                <Link to="/venus">
+                  <div className="venus"></div>    
+                </Link>
 
-			
-			<Link to="/mercury">
-        <div className="mercury"></div>	
-			</Link>
+                <Link to="/earth">
+                  <div className="earth"></div> 
+                </Link>
 
-			<Link to="/venus">
-        <div className="venus"></div>    
-			</Link>
+                <Link to="/mars">
+                  <div className="mars"></div> 
+                </Link>
 
-			<Link to="/earth">
-        <div className="earth"></div> 
-			</Link>
+                <Link to="/jupyter">
+                  <div className="jupyter"></div>
+                </Link>
 
-      <Link to="/mars">
-        <div className="mars"></div> 
-			</Link>
+                <Link to="/saturn">
+                  <div className="saturn">
+                  <div className="rings"></div>
+                  <div className="tops"></div>
+                  </div>
+                </Link>
 
-			<Link to="/jupyter">
-        <div className="jupyter"></div>
-			</Link>
+                <Link to="/uranus">
+                  <div className="uranus">
+                  <div className="ringu"></div>
+                  <div className="topu"></div>
+                  </div>
+                </Link>
 
-			<Link to="/saturn">
-				<div className="saturn">
-				<div className="rings"></div>
-				<div className="tops"></div>
-				</div>
-			</Link>
-
-			<Link to="/uranus">
-				<div className="uranus">
-				<div className="ringu"></div>
-				<div className="topu"></div>
-				</div>
-			</Link>
-
-			<Link to="/neptune">
-        <div className="neptune"></div>
-			</Link>
+                <Link to="/neptune">
+                  <div className="neptune"></div>
+                </Link>
+            </div>
+            
         </div>
     </div>
   )
