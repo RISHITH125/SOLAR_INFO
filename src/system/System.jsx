5f4c686f9642ea176794ded20af4ff6759a76861
RISHITH125 <rishithp125@gmail.com>
2023-10-23 17:47:08 +0530

    minor positioning changes

--- src/system/System.jsx
+++ src/system/System.jsx
@@ -6,46 +6,46 @@ import { Link } from 'react-router-dom'
 
 const System = () => {
   const main = useRef(null); // main is like a pointer | main.current = *main
-  const [scaleFactor, setScaleFactor] = useState(100);
-  const [translateFactor, setTranslateFactor] = useState(0);
+  const [scaleFactor, setScaleFactor] = useState(1000);
+  const [translateFactor, setTranslateFactor] = useState(-2);
   const [opacity, setOpacity] = useState(1);
 
   let mainStyle = {
     transform: `scale(${scaleFactor}%) translateX(${translateFactor}%)`
   }
 
-  // let opacityClass = {
-  //   opacity: `${opacity}`
-  // }
+  let opacityClass = {
+    opacity: `${opacity}`
+  }
 
-  // useEffect(()=>{
-  //   main.current.addEventListener('wheel', (e)=>{
-  //       if (e.deltaY > 0) {
-  //         setScaleFactor((current) => {
-  //           console.log(current)
-  //           if (current < 100) return current;
-  //           setTranslateFactor((c) => c + 0.01);
-  //           setOpacity(o => o - 0.01)
-  //           return current - 10;
-  //         })
-  //       } else {
-  //         setScaleFactor((current) => {
-  //           if (current < 100 || current > 1100) return current;
-  //           setTranslateFactor((c) => c - 0.01);
-  //           setOpacity(o => o + 0.01)
-  //           return current + 10;
-  //         })
+  useEffect(()=>{
+    main.current.addEventListener('wheel', (e)=>{
+        if (e.deltaY > 0) {
+          setScaleFactor((current) => {
+            console.log(current)
+            if (current < 100) return current;
+            setTranslateFactor((c) => c + 0.01);
+            setOpacity(o => o - 0.01)
+            return current - 10;
+          })
+        } else {
+          setScaleFactor((current) => {
+            if (current < 100 || current > 1100) return current;
+            setTranslateFactor((c) => c - 0.01);
+            setOpacity(o => o + 0.01)
+            return current + 10;
+          })
           
-  //       }
-  //   })
-  // }, [main])
+        }
+    })
+  }, [main])
 
   return ( 
     <div className=''>
         <div ref={main} className={`main flex justify-center items-center`} style={mainStyle}>
             <div className="sun flex absolute items-center">
-              {/* <h1 className={`relative font-thin text-white left-[3vw] info`} style={opacityClass}>SOLAR</h1>
-              <h1 className={`relative text-white left-[4vw] info`} style={opacityClass}>INFO</h1> */}
+              <h1 className={`relative font-thin text-white left-[3vw] info`} style={opacityClass}>SOLAR</h1>
+              <h1 className={`relative text-white left-[4vw] info`} style={opacityClass}>INFO</h1>
               <Link to="/sun">
               <div className="sun-shadow"></div>
               </Link>
