5f9a17bd16c79e9b645e0cefc0cb828848c19451
RISHITH125 <rishithp125@gmail.com>
2023-10-29 03:05:02 +0530

    UI changes trial

--- src/system/UserInterface.jsx
+++ src/system/UserInterface.jsx
@@ -3,7 +3,7 @@ import './ui.css'
 function Ui() {
   return (
     <div className={`Sample`}>
-      <p className={`relative text-xl italic font-bold  text-white`}>USER INTERFACE</p>
+      
     </div>
     
   )
