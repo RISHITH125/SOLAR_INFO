085e83d8a0dc5fda6bc3f70b9e2003ed6e38a195
RISHITH125 <rishithp125@gmail.com>
2023-11-06 22:39:36 +0530

    minor UI changes

--- src/system/UserInterface.jsx
+++ src/system/UserInterface.jsx
@@ -1,15 +1,84 @@
+// import React, { useEffect, useState } from 'react';
+// import './ui.css';
+// import { Link } from 'react-router-dom'
+// import { menu } from '../assets';
+
+// function Ui() {
+//   const [isclicked, setIsClicked] = useState(false);
+//   const [maxwidth, setMaxWidth] = useState(window.innerWidth);
+//   const handleClick = () => {
+//     setIsClicked(!isclicked);
+//   };
+
+//   let closesymb = window.innerWidth > 800 ? "Close cursor-pointer w-[28vw] flex flex-row justify-end " : "Close cursor-pointer";
+//   useEffect(() => {
+//     const handleResize = () => {
+//       setMaxWidth(window.innerWidth);
+//     };
+//     window.addEventListener('resize', handleResize);
+//     return () => {
+//       window.removeEventListener('resize', handleResize);
+//     };
+//   }, []);
+
+//   return (
+//     <div className={`UserInter`}>
+//         <div className={`interact text-white font-bold from-neutral-50`} >
+//           {isclicked ? (
+//             <div className={`interact-tab flex flex-col justify-start items-center gap-1`} >
+//               <div className={closesymb} onClick={handleClick}>
+//                 {<img className='w-[2vw] hover:border-2 hover:border-orange-800' src={menu}></img> && (maxwidth > 800)}
+//               </div>
+//               <div className="w-[26vw] h-[90vh] text-white flex flex-col justify-start items-center gap-5">
+//                 <Link className='hover:text-orange-500' to="/sun">Sun</Link>
+//                 <Link className='hover:text-orange-500' to="/mercury">Mercury</Link>
+//                 <Link className='hover:text-orange-500' to="/venus">Venus</Link>
+//                 <Link className='hover:text-orange-500' to="/earth">Earth</Link>
+//                 <Link className='hover:text-orange-500' to="/mars">Mars</Link>
+//                 <Link className='hover:text-orange-500' to="/jupiter">Jupiter</Link>
+//                 <Link className='hover:text-orange-500' to="/uranus">Uranus</Link>
+//                 <Link className='hover:text-orange-500' to="/neptune">Neptune</Link>
+//               </div>
+//             </div>
+//           ) : (maxwidth > 800) ? (
+//             <div className='absolute cursor-pointer font-bold text-xl italic top-5 left-7 Menu' onClick={handleClick}>
+//               <div className={`Menu_line1`}></div>
+//               <div className={`Menu_line2`}></div>
+//               <div className={`Menu_line3`}></div>
+//             </div>
+//           ) : (
+//             <div className={`cursor-pointer Open`} onClick={handleClick}>
+              
+//             </div>
+//           )}
+//         </div>
+          
+//         <div className='about'>
+//             <Link to="/About">
+//               <div className={`font-sans text-lg text-white`}>ABOUT</div>
+//             </Link> 
+//         </div>
+//     </div>
+//   );
+// }
+
+// export default Ui;
+
 import React, { useEffect, useState } from 'react';
 import './ui.css';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { menu } from '../assets';
 
 function Ui() {
   const [isclicked, setIsClicked] = useState(false);
   const [maxwidth, setMaxWidth] = useState(window.innerWidth);
+
   const handleClick = () => {
     setIsClicked(!isclicked);
   };
 
+  let closesymb = window.innerWidth > 800 ? "Close cursor-pointer w-[28vw] flex flex-row justify-end" : "Close cursor-pointer";
+  let Interacttab =window.innerWidth > 800 ? "interact-tab flex flex-col justify-start items-center gap-10":"interact-tab flex flex-col justify-start items-center gap-4"
   useEffect(() => {
     const handleResize = () => {
       setMaxWidth(window.innerWidth);
@@ -22,41 +91,46 @@ function Ui() {
 
   return (
     <div className={`UserInter`}>
-        <div className={`interact text-white font-bold from-neutral-50`} >
-          {isclicked ? (
-            <div className={`interact-tab flex flex-col justify-start items-center gap-5`} >
-              <div className={`Close cursor-pointer w-[28.5vw] flex flex-row justify-end`} onClick={handleClick}>
-                <img className='w-[2vw] hover:border-2 hover:border-orange-800' src={menu}></img>
+      <div className={`interact text-white font-bold from-neutral-50`}>
+        {isclicked ? (
+          <div className={Interacttab}>
+            {maxwidth > 800 ? (
+              <div className={closesymb} onClick={handleClick}>
+                <img className='w-[2vw] hover:border-2 hover:border-orange-800' src={menu} alt="Menu" />
               </div>
-              <div className="w-[26vw] h-[90vh] text-white flex flex-col justify-start items-center gap-5">
-                <Link className='hover:text-orange-500' to="/sun">Sun</Link>
-                <Link className='hover:text-orange-500' to="/mercury">Mercury</Link>
-                <Link className='hover:text-orange-500' to="/venus">Venus</Link>
-                <Link className='hover:text-orange-500' to="/earth">Earth</Link>
-                <Link className='hover:text-orange-500' to="/mars">Mars</Link>
-                <Link className='hover:text-orange-500' to="/jupiter">Jupiter</Link>
-                <Link className='hover:text-orange-500' to="/uranus">Uranus</Link>
-                <Link className='hover:text-orange-500' to="/neptune">Neptune</Link>
+            ) : (
+              <div className={closesymb} onClick={handleClick}>
               </div>
+            )}
+            <div className="w-[25vw] h-[90vh] text-white flex flex-col justify-start items-center gap-5">
+              <Link className='hover:text-orange-500' to="/sun">Sun</Link>
+              <Link className='hover:text-orange-500' to="/mercury">Mercury</Link>
+              <Link className='hover:text-orange-500' to="/venus">Venus</Link>
+              <Link className='hover:text-orange-500' to="/earth">Earth</Link>
+              <Link className='hover:text-orange-500' to="/mars">Mars</Link>
+              <Link className='hover:text-orange-500' to="/jupiter">Jupiter</Link>
+              <Link className='hover:text-orange-500' to="/uranus">Uranus</Link>
+              <Link className='hover:text-orange-500' to="/neptune">Neptune</Link>
             </div>
-          ) : (maxwidth > 800) ? (
-            <div className='absolute cursor-pointer font-bold text-xl italic top-5 left-7 Menu' onClick={handleClick}>
-              <div className={`Menu_line1`}></div>
-              <div className={`Menu_line2`}></div>
-              <div className={`Menu_line3`}></div>
-            </div>
-          ) : (
-            <div className={`cursor-pointer Open`} onClick={handleClick}>
-              
-            </div>
-          )}
-        </div>
-          
-        <div className='about'>
-            <Link to="/About">
-              <div className={`font-sans text-lg text-white`}>ABOUT</div>
-            </Link> 
-        </div>
+          </div>
+        ) : (maxwidth > 800) ? (
+          <div className='absolute cursor-pointer font-bold text-xl italic top-5 left-7 Menu' onClick={handleClick}>
+            <div className={`Menu_line1`}></div>
+            <div className={`Menu_line2`}></div>
+            <div className={`Menu_line3`}></div>
+          </div>
+        ) : (
+          <div className={`cursor-pointer Open`} onClick={handleClick}>
+            {/* Content for the "Open" state */}
+          </div>
+        )}
+      </div>
+
+      <div className='about'>
+        <Link to="/About">
+          <div className={`font-sans text-lg text-white`}>ABOUT</div>
+        </Link>
+      </div>
     </div>
   );
 }
--- src/system/profilessss/Profile.jsx
+++ src/system/profilessss/Profile.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+
 function Profile(p) {
+
+  let s = {
+    backgroundImage: `url('${p.url}')`
+  }
+
   let ifrotate="180deg"
   return (
     <>
         <div className={`rounded-l-full w-[96.5vw] h-[30vh] profile -rotate-[${ (+(p.rotate)) && ifrotate}]`} style={p.styles}>
+            <div className='rounded-full border-2 border-red-800 ml-16 mt-5 w-[10vw] h-[10vh]' style={s}>
+              {/* <img src={p.dataImage} alt={p.alt} />     */}
+            </div>
             {/* <div className=''></div>
             <div className=''></div> */}
         </div>
